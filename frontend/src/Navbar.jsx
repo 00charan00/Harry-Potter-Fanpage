eebd808b1af21fd71293405b357caa044683074f
00charan00 <poycharan@gmail.com>
2024-02-04 14:23:17 +0530

    dev commit

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,42 +1,15 @@
-// Navbar.js
+// Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        {/* Logo on the left */}
-        <Link to="/" className="text-white text-xl font-bold">YourLogo</Link>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-gray-800 p-4 text-white">
+      {/* Your Navbar content, such as links */}
+      <img src="./assets/hp.png" alt="" />
 
-        {/* Dropdown list 1 */}
-        <div className="relative inline-block text-left">
-          <button className="text-white hover:text-gray-300 focus:outline-none">
-            Dropdown 1
-          </button>
-          <div className="hidden absolute z-10 mt-2 bg-gray-800 text-white">
-            {/* Dropdown content */}
-            <Link to="/dropdown1-item1" className="block px-4 py-2">Dropdown 1 - Item 1</Link>
-            <Link to="/dropdown1-item2" className="block px-4 py-2">Dropdown 1 - Item 2</Link>
-          </div>
-        </div>
-
-        {/* Dropdown list 2 */}
-        <div className="relative inline-block text-left">
-          <button className="text-white hover:text-gray-300 focus:outline-none">
-            Dropdown 2
-          </button>
-          <div className="hidden absolute z-10 mt-2 bg-gray-800 text-white">
-            {/* Dropdown content */}
-            <Link to="/dropdown2-item1" className="block px-4 py-2">Dropdown 2 - Item 1</Link>
-            <Link to="/dropdown2-item2" className="block px-4 py-2">Dropdown 2 - Item 2</Link>
-          </div>
-        </div>
-
-        {/* Additional link elements */}
-        <Link to="/link1" className="text-white hover:text-gray-300">Link 1</Link>
-        <Link to="/link2" className="text-white hover:text-gray-300 ml-4">Link 2</Link>
-      </div>
+      <Link to="/home" className="text-white mr-4">Home</Link>
+      <Link to="/letter" className="text-white">Letter</Link>
     </nav>
   );
 };
